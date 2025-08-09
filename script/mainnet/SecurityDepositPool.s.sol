4657e076e794ad0ede59825677ce878b3c071374
9oelM <hj923@hotmail.com>
2025-08-09 23:01:40 +0900

    feat: mainnet deployment

--- script/mainnet/SecurityDepositPool.s.sol
+++ script/mainnet/SecurityDepositPool.s.sol
@@ -15,18 +15,25 @@ contract SecurityDepositPoolScript is Script {
     function run() public {
         vm.startBroadcast();
 
+        // Joel https://etherscan.io/address/0x8Fe7A21fe057F9c31812e5049128a41fea79d066
         address instructor = 0x8Fe7A21fe057F9c31812e5049128a41fea79d066;
-        // BRL
+        // BRL https://etherscan.io/address/0x2a063d9C09a5C5fAdB53d67F298D650f371bADb5
         address fundsManager = 0x2a063d9C09a5C5fAdB53d67F298D650f371bADb5;
         // Backup funds manager address. We're gonna use the slashed funds ourselves anyways,
         // so it's ok to set it to the instructor address
         address backupFundsManager = instructor;
         // 73 USDT
         uint256 flatDepositAmount = 73 * 10 ** 6;
-        // Current approx time =
-        uint256 courseEndTime = block.timestamp + 24 hours;
+
+        // Approx deployment time = Sat Aug 09 2025 23:00:00 GMT+0900
+        // Math.floor(new Date("2025-10-04T23:00:00+09:00").getTime() / 1000)
+        uint256 courseEndTime = 1759586400;
+        // Sanity check
+        // 7 weeks till Oct 04, so must be 58 days away at max
+        require(courseEndTime < block.timestamp + 60 days, "Course end time too late");
+        require(courseEndTime > block.timestamp, "Course end time too early");
         pool = new SecurityDepositPool(
-            instructor, fundsManager, backupFundsManager, address(usdt), flatDepositAmount, courseEndTime
+            instructor, fundsManager, backupFundsManager, usdt, flatDepositAmount, courseEndTime
         );
 
         vm.stopBroadcast();
