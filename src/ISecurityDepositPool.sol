8997894172c9520bbbd2f678a5109ea0f43b7c86
9oelM <hj923@hotmail.com>
2025-08-09 22:50:26 +0900

    fix: remove indexed on address array

--- src/ISecurityDepositPool.sol
+++ src/ISecurityDepositPool.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.30;
 
 interface ISecurityDepositPool {
     event Deposited(address indexed student, uint256 amount);
-    event SlashedMany(address[] indexed students, uint256[] amounts);
+    event SlashedMany(address[] students, uint256[] amounts);
     event Withdrawn(address indexed student);
-    event WithdrawnMany(address[] indexed students);
+    event WithdrawnMany(address[] students);
     event SlashedTransferred(address indexed fundsManager, uint256 amount);
 }
